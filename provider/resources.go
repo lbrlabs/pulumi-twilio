b3aceae59cb91dad8fccd83ec992b3ad78282e8e
Lee Briggs <lee@leebriggs.co.uk>
2022-12-27 09:20:29 +0000

    initial commit with resource mappings

--- provider/resources.go
+++ provider/resources.go
@@ -1,39 +1,55 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package xyz
+package twilio
 
 import (
 	"fmt"
 	"path/filepath"
+	"unicode"
 
+	"github.com/lbrlabs/pulumi-twilio/provider/pkg/version"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
-	"github.com/pulumi/pulumi-xyz/provider/pkg/version"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
-	"github.com/terraform-providers/terraform-provider-xyz/xyz"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
+	"github.com/twilio/terraform-provider-twilio/twilio"
 )
 
 // all of the token components used below.
 const (
 	// This variable controls the default name of the package in the package
 	// registries for nodejs and python:
-	mainPkg = "xyz"
+	mainPkg = "twilio"
 	// modules:
-	mainMod = "index" // the xyz module
+	mainMod        = "index" // the twilio module
+	accountsMod    = "accounts"
+	apiAccountsMod = "apiaccounts"
 )
+
+// twilioMember manufactures a type token for the Twilio package and the given module and type.
+func twilioMember(mod string, mem string) tokens.ModuleMember {
+	return tokens.ModuleMember(mainPkg + ":" + mod + ":" + mem)
+}
+
+// scalewayType manufactures a type token for the Twilio package and the given module and type.
+func twilioType(mod string, typ string) tokens.Type {
+	return tokens.Type(twilioMember(mod, typ))
+}
+
+// twilioDataSource manufactures a standard resource token given a module and resource name.
+// It automatically uses the Twilio package and names the file by simply lower casing the data
+// source's first character.
+func twilioDataSource(mod string, res string) tokens.ModuleMember {
+	fn := string(unicode.ToLower(rune(res[0]))) + res[1:]
+	return twilioMember(mod+"/"+fn, res)
+}
+
+// twilioResource manufactures a standard resource token given a module and resource name.
+// It automatically uses the Twilio package and names the file by simply lower casing the resource's
+// first character.
+func twilioResource(mod string, res string) tokens.Type {
+	fn := string(unicode.ToLower(rune(res[0]))) + res[1:]
+	return twilioType(mod+"/"+fn, res)
+}
 
 // preConfigureCallback is called before the providerConfigure function of the underlying provider.
 // It should validate that the provider can be configured, and provide actionable errors in the case
@@ -46,42 +62,23 @@ func preConfigureCallback(vars resource.PropertyMap, c shim.ResourceConfig) erro
 // Provider returns additional overlaid schema and metadata associated with the provider..
 func Provider() tfbridge.ProviderInfo {
 	// Instantiate the Terraform provider
-	p := shimv2.NewProvider(xyz.Provider())
+	p := shimv2.NewProvider(twilio.Provider())
 
 	// Create a Pulumi provider mapping
 	prov := tfbridge.ProviderInfo{
-		P:    p,
-		Name: "xyz",
-		// DisplayName is a way to be able to change the casing of the provider
-		// name when being displayed on the Pulumi registry
-		DisplayName: "",
-		// The default publisher for all packages is Pulumi.
-		// Change this to your personal name (or a company name) that you
-		// would like to be shown in the Pulumi Registry if this package is published
-		// there.
-		Publisher: "Pulumi",
-		// LogoURL is optional but useful to help identify your package in the Pulumi Registry
-		// if this package is published there.
-		//
-		// You may host a logo on a domain you control or add an SVG logo for your package
-		// in your repository and use the raw content URL for that file as your logo URL.
-		LogoURL: "",
-		// PluginDownloadURL is an optional URL used to download the Provider
-		// for use in Pulumi programs
-		// e.g https://github.com/org/pulumi-provider-name/releases/
-		PluginDownloadURL: "",
-		Description:       "A Pulumi package for creating and managing xyz cloud resources.",
-		// category/cloud tag helps with categorizing the package in the Pulumi Registry.
-		// For all available categories, see `Keywords` in
-		// https://www.pulumi.com/docs/guides/pulumi-packages/schema/#package.
-		Keywords:   []string{"pulumi", "xyz", "category/cloud"},
-		License:    "Apache-2.0",
-		Homepage:   "https://www.pulumi.com",
-		Repository: "https://github.com/pulumi/pulumi-xyz",
-		// The GitHub Org for the provider - defaults to `terraform-providers`. Note that this
-		// should match the TF provider module's require directive, not any replace directives.
-		GitHubOrg: "",
-		Config:    map[string]*tfbridge.SchemaInfo{
+		P:                 p,
+		Name:              "twilio",
+		DisplayName:       "Twilio",
+		Publisher:         "lbrlabs",
+		LogoURL:           "",
+		PluginDownloadURL: "github://api.github.com/lbrlabs",
+		Description:       "A Pulumi package for creating and managing Twilio cloud resources.",
+		Keywords:          []string{"pulumi", "twilio", "category/cloud"},
+		License:           "Apache-2.0",
+		Homepage:          "https://leebriggs.co.uk/projects#pulumi-twilio",
+		Repository:        "https://github.com/lbrlabs/pulumi-twilio",
+		GitHubOrg:         "twilio",
+		Config:            map[string]*tfbridge.SchemaInfo{
 			// Add any required configuration here, or remove the example below if
 			// no additional points are required.
 			// "region": {
@@ -92,19 +89,32 @@ func Provider() tfbridge.ProviderInfo {
 			// },
 		},
 		PreConfigureCallback: preConfigureCallback,
-		Resources:            map[string]*tfbridge.ResourceInfo{
-			// Map each resource in the Terraform provider to a Pulumi type. Two examples
-			// are below - the single line form is the common case. The multi-line form is
-			// needed only if you wish to override types or other default options.
-			//
-			// "aws_iam_role": {Tok: tfbridge.MakeResource(mainPkg, mainMod, "IamRole")}
-			//
-			// "aws_acm_certificate": {
-			// 	Tok: tfbridge.MakeResource(mainPkg, mainMod, "Certificate"),
-			// 	Fields: map[string]*tfbridge.SchemaInfo{
-			// 		"tags": {Type: tfbridge.MakeType(mainPkg, "Tags")},
-			// 	},
-			// },
+		Resources: map[string]*tfbridge.ResourceInfo{
+			"twilio_accounts_credentials_aws_v1":                                          {Tok: twilioResource(accountsMod, "CredentialsAwsV1")},
+			"twilio_accounts_credentials_public_keys_v1":                                  {Tok: twilioResource(accountsMod, "CredentialsPublicKeysV1")},
+			"twilio_api_accounts_addresses":                                               {Tok: twilioResource(apiAccountsMod, "Addresses")},
+			"twilio_api_accounts_applications":                                            {Tok: twilioResource(apiAccountsMod, "Applications")},
+			"twilio_api_accounts_calls":                                                   {Tok: twilioResource(apiAccountsMod, "Calls")},
+			"twilio_api_accounts_calls_feedback_summary":                                  {Tok: twilioResource(apiAccountsMod, "CallsFeedbackSummary")},
+			"twilio_api_accounts_calls_recordings":                                        {Tok: twilioResource(apiAccountsMod, "CallsRecordings")},
+			"twilio_api_accounts_conferences_participants":                                {Tok: twilioResource(apiAccountsMod, "ConferencesParticipans")},
+			"twilio_api_accounts_incoming_phone_numbers":                                  {Tok: twilioResource(apiAccountsMod, "IncomingPhoneNumbers")},
+			"twilio_api_accounts_incoming_phone_numbers_assigned_add_ons":                 {Tok: twilioResource(apiAccountsMod, "IncomingPhoneNumbersAssignedAddons")},
+			"twilio_api_accounts_keys":                                                    {Tok: twilioResource(apiAccountsMod, "Keys")},
+			"twilio_api_accounts_messages":                                                {Tok: twilioResource(apiAccountsMod, "Messages")},
+			"twilio_api_accounts_queues":                                                  {Tok: twilioResource(apiAccountsMod, "Queues")},
+			"twilio_api_accounts_signing_keys":                                            {Tok: twilioResource(apiAccountsMod, "SigningKeys")},
+			"twilio_api_accounts_sip_credential_lists":                                    {Tok: twilioResource(apiAccountsMod, "SipCredentialsLists")},
+			"twilio_api_accounts_sip_credential_lists_credentials":                        {Tok: twilioResource(apiAccountsMod, "SipCredentialsListsCredentials")},
+			"twilio_api_accounts_sip_domains":                                             {Tok: twilioResource(apiAccountsMod, "SipDomains")},
+			"twilio_api_accounts_sip_domains_auth_calls_credential_list_mappings":         {Tok: twilioResource(apiAccountsMod, "SipDomainsAuthCallsCredentialListMappings")},
+			"twilio_api_accounts_sip_domains_auth_calls_ip_access_control_list_mappings":  {Tok: twilioResource(apiAccountsMod, "SipDomainsAuthCallsIpAccessControlListMappings")},
+			"twilio_api_accounts_sip_domains_auth_registrations_credential_list_mappings": {Tok: twilioResource(apiAccountsMod, "SipDomainsAuthRegistrationsCredentialListMappings")},
+			"twilio_api_accounts_sip_domains_credential_list_mappings":                    {Tok: twilioResource(apiAccountsMod, "SipCredentialsListsCredentials")},
+			"twilio_api_accounts_sip_domains_ip_access_control_list_mappings":             {Tok: twilioResource(apiAccountsMod, "SipDomainsIpAccessControlListMappings")},
+			"twilio_api_accounts_sip_ip_access_control_lists":                             {Tok: twilioResource(apiAccountsMod, "SipIpAccessControlLists")},
+			"twilio_api_accounts_sip_ip_access_control_lists_ip_addresses":                {Tok: twilioResource(apiAccountsMod, "SipIpAccessControlListsIpAddresses")},
+			"twilio_api_accounts_usage_triggers":                                          {Tok: twilioResource(apiAccountsMod, "UsageTriggers")},
 		},
 		DataSources: map[string]*tfbridge.DataSourceInfo{
 			// Map each resource in the Terraform provider to a Pulumi function. An example
